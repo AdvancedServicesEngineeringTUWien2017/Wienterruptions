81c1c62fc56aa8d8e16782eae3c3481e4bde468c
Julian Pfanner <julian.pfanner@gmail.com>
2017-06-19 13:02:12 +0200

    added dummy implementation for eventhub sender and receiver

---
+++ MiniProject/EventHubReceiver/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.EventHubs;
+using Microsoft.Azure.EventHubs.Processor;
+
+namespace EventHubReceiver
+{
+    class Program
+    {
+        private const string EhConnectionString = "Endpoint=sb://sensorinputevents.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=qZUuYmnG/P7WWjFcQ1V1voFWzL5l8HTsOAj54ozKDGI=";
+        private const string EhEntityPath = "sensoroutput";
+        private const string StorageContainerName = "asewinerliniencontainer";
+        private const string StorageAccountName = "asewienerlinienstorage";
+        private const string StorageAccountKey = "LbDH/+nnRAGq9QrRYh3aAv4DokCP93zIpm5zmn/6W3wRwgszGl6yDIi1CojN10dM5nx3fUGIZkHzaG70ngaANQ==";
+
+        private static readonly string StorageConnectionString =
+            string.Format(
+                "DefaultEndpointsProtocol=https;AccountName={0};AccountKey={1}",
+                StorageAccountName,
+                StorageAccountKey);
+
+        static void Main(string[] args)
+        {
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        private static async Task MainAsync(string[] args)
+        {
+            Console.WriteLine("Registering EventProcessor...");
+
+            var eventProcessorHost = new EventProcessorHost(
+                EhEntityPath,
+                PartitionReceiver.DefaultConsumerGroupName,
+                EhConnectionString,
+                StorageConnectionString,
+                StorageContainerName);
+
+            await eventProcessorHost.RegisterEventProcessorAsync<SimpleEventProcessor>();
+
+            Console.WriteLine("Receiving. Press ENTER to stop worker...");
+            Console.ReadLine();
+
+            await eventProcessorHost.UnregisterEventProcessorAsync();
+        }
+    }
+}
---
+++ MiniProject/Sensor/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.EventHubs;
+
+namespace Sensor
+{
+    class Program
+    {
+        private static EventHubClient eventHubClient;
+        //TODO proper configuration
+        private const string EhConnectionString = "Endpoint=sb://sensorinputevents.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=qZUuYmnG/P7WWjFcQ1V1voFWzL5l8HTsOAj54ozKDGI=";
+
+        private const string EhEntityPath = "sensorinput";
+
+        static void Main(string[] args)
+        {
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        private static async Task MainAsync(string[] args)
+        {
+            var connectionStringBuilder = new EventHubsConnectionStringBuilder(EhConnectionString)
+            {
+                EntityPath = EhEntityPath
+            };
+
+            eventHubClient = EventHubClient.CreateFromConnectionString(connectionStringBuilder.ToString());
+
+            await SendMessagesToEventHub(15);
+
+            await eventHubClient.CloseAsync();
+
+            Console.WriteLine("Press ENTER to exit...");
+            Console.ReadLine();
+        }
+
+        private static async Task SendMessagesToEventHub(int numMessagesToSend)
+        {
+            for (var i = 0; i < numMessagesToSend; i++)
+            {
+                try
+                {
+                    var message = $"{{id: {i}, message: 'Message {i}'}}";
+                    Console.WriteLine($"Sending message: {message}");
+                    await eventHubClient.SendAsync(new EventData(Encoding.UTF8.GetBytes(message))).ConfigureAwait(false);
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine($"{DateTime.Now} > Exception: {exception.Message}");
+                }
+
+                await Task.Delay(10);
+            }
+
+            Console.WriteLine($"{numMessagesToSend} messages sent.");
+        }
+    }
+}
